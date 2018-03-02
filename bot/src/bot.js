97ae1e7d6d3afebb8e72d0350b29a5ce64db2e61
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-02 14:35:28 -0800

    added phrase based hearing to bot

--- bot/src/bot.js
+++ bot/src/bot.js
@@ -2,10 +2,25 @@ var botkit = require('botkit');
 const {API_TOKEN} = require('../config/config')
 var controller = botkit.slackbot({ debug : false });
 var bot = controller.spawn({token : API_TOKEN});
+
 // connect to slack real time messaging API
 bot.startRTM();
-// listen and respond to user messages
+
+// simple listen and respond to user messages
 controller.on('direct_message', function(bot, message){
   console.log(message);
-  bot.reply(message, 'roger that, Cap !')
+  bot.reply(message, 'Roger that, Cap !')
+});
+
+// listen to specific phrases
+controller.hears(['hi','hello'],'direct_message', function(bot, message){
+  bot.reply(message, 'Hi, How are you ?');
+});
+
+controller.hears(['how are you'],'direct_message', function(bot,message){
+  bot.reply(message, 'I am doing amazing. How about you?');
+});
+
+controller.hears(['doing','good','great'],'direct_message', function(bot, message){
+  bot.reply(message, 'Well, I am glad to hear that. ')
 });
