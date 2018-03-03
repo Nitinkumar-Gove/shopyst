8ade03cc5f381b42870e5f72088b3e6ab6b0da0b
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-03 10:01:40 -0800

    added simple response tokens

--- bot/config/config.js
+++ bot/config/config.js
@@ -1,3 +1,4 @@
+const API_TOKEN = "xoxb-324383634135-jqqw8ueKLXazQqwNsFMDpuTG" ;
 module.exports = {
   API_TOKEN
 };
--- bot/src/bot.js
+++ bot/src/bot.js
@@ -1,26 +1,21 @@
 var botkit = require('botkit');
-const {API_TOKEN} = require('../config/config')
+const {API_TOKEN} = require('../config/config');
 var controller = botkit.slackbot({ debug : false });
 var bot = controller.spawn({token : API_TOKEN});
+var responses = require('../config/responses');
 
 // connect to slack real time messaging API
 bot.startRTM();
 
-// simple listen and respond to user messages
-controller.on('direct_message', function(bot, message){
-  console.log(message);
-  bot.reply(message, 'Roger that, Cap !')
-});
-
 // listen to specific phrases
-controller.hears(['hi','hello'],'direct_message', function(bot, message){
-  bot.reply(message, 'Hi, How are you ?');
+controller.hears(['hi','hello','hey'],'direct_message', function(bot, message){
+  bot.reply(message, responses.HOW_ARE_YOU);
 });
 
 controller.hears(['how are you'],'direct_message', function(bot,message){
-  bot.reply(message, 'I am doing amazing. How about you?');
+  bot.reply(message, responses.DOING_AMAZING);
 });
 
 controller.hears(['doing','good','great'],'direct_message', function(bot, message){
-  bot.reply(message, 'Well, I am glad to hear that. ')
+  bot.reply(message, responses.GLAD_TO_HEAR)
 });
