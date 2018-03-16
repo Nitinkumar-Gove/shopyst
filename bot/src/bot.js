7f8bb8d5cada1c322a7abca0f57b88549000a08a
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-16 00:16:39 -0700

    added conversation handlers

--- bot/src/bot.js
+++ bot/src/bot.js
@@ -1,38 +1,53 @@
 var botkit = require('botkit');
+var response = require('../config/responses');
+var messages = require('../config/messages');
+
 const {API_TOKEN} = require('../config/config');
 var controller = botkit.slackbot({ debug : false });
 var bot = controller.spawn({token : API_TOKEN});
-var response = require('../config/responses');
-var message = require('../config/messages')
 
-// connect to slack real time messaging API
 bot.startRTM();
 
-// listen to specific phrases
-controller.hears([message.HI,message.HELLO,message.HEY],'direct_message', function(bot, message){
-  bot.reply(message, response.HOW_ARE_YOU);
+controller.hears([messages.HI, messages.HELLO, messages.HEY, messages.THERE], 'direct_message', function(bot, message){
+  bot.createConversation(message, function(error, convo){
+    convo.say(messages.HELLO);
+    convo.activate();
+  });
 });
 
-controller.hears([message.HOW_ARE_yOU],'direct_message', function(bot,message){
-  bot.reply(message, response.DOING_AMAZING);
+controller.hears(['how are you'],'direct_message' ,function(bot, message){
+  bot.startConversation(message, function(error, convo){
+    convo.say('I am doing amazing. thank you.');
+    convo.next();
+  });
 });
 
-controller.hears([message.DOING,message.GOOD,message.GREAT],'direct_message', function(bot, message){
-  bot.reply(message, response.GLAD_TO_HEAR)
+controller.hears(['\\b(?:help)\\b'], 'direct_message', function(bot, message){
+   bot.startConversation(message, function(error, convo){
+           convo.say('how can I help you today? ');
+           convo.next();
+   });
 });
 
-controller.hears([message.HELP],'direct_message', function(bot, message){
-  bot.reply(message, response.HOW_CAN_I_HELP)
-});
+controller.hears(['\\b(?:buy|search|look|look|find)\\b'], 'direct_message', function(bot, message){
+   bot.startConversation(message, function(error, convo){
 
-controller.hears([message.BYE],'direct_message', function(bot, message){
-  bot.reply(message, response.BYE)
-});
+           convo.say('sure, I can help you with that');
 
-controller.hears(['options','skills','functions','what can you do'],'direct_message', function(bot, message){
-  bot.reply(message, response.MENU)
-});
+           convo.addQuestion('is there any specific brand you are looking for?', function(res, convo) {
+                convo.gotoThread('q2')
+            }, {}, 'default')
 
-controller.hears('I want to buy (.*)','direct_message',function(bot,message){
-  bot.reply(message, 'sure, let me look it up for you.')
-})
+            convo.addQuestion('what is your price limit?', function(res, convo) {
+                 convo.gotoThread('q3')
+             }, {}, 'q2')
+
+             convo.addQuestion('any specific color you want it in?', function(res, convo) {
+                  convo.gotoThread('end')
+              }, {}, 'q3')
+
+              convo.addMessage('gotch, thank you for details.', 'end')
+
+           convo.next();
+   });
+});
