2f381a1b178812e345947748eec80af45dd74f83
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-26 23:30:01 -0700

    adding simple implementation using microsoft chat builder

---
+++ bot/src/app.js
@@ -0,0 +1,22 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+
+// setup restify server
+var server = restify.createServer();
+server.listen(1000, function(){
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+  appId : '',
+  appPassword : ''
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function(session){
+  session.send("You said: %s", session.message.text);
+});
