55e90a5cd6dbfb6756c88e83ff74c280787e2e90
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-31 14:43:15 -0700

    added simple dialog to bot

--- bot/src/app.js
+++ bot/src/app.js
@@ -17,6 +17,26 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function(session){
-  session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector, [
+    // root controller dialog
+    (session, args, next) => {
+        session.send("Hello, I am shopyst");
+        session.send("I can help you find the best products.")
+        session.beginDialog('getProduct');
+  }]);
+
+  // simple dialog to get product user is looking for
+  bot.dialog('getProduct',[
+    (session, args, next) => {
+      builder.Prompts.text(session, 'what are you looking for today ?');
+    },
+    (session, results , next) => {
+      productName = session.dialogData.productName = results.response;
+      if(!productName || productName.trim().length == 0 ){
+        session.send("Sorry, I didnt get you")
+      }
+      else{
+        session.send("great, let me look it up for you")
+      }
+    }
+  ]);
