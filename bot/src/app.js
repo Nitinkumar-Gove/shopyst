7734cb8cc0b86143a702525ba3bb374d3854f0e5
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-03-31 17:13:43 -0700

    added getMinimumRating dialog to bot

--- bot/src/app.js
+++ bot/src/app.js
@@ -23,32 +23,53 @@
         session.send("I can help you find the best products.")
         session.beginDialog('getProduct');
   },
+  // extract productName
   (session, results, next) => {
     /* design note :
     *  once we recieve the input from the user , we should store it in a
     *  'requirement-kind-of' json object which can be used during product search
     */
+    console.log("[ LOGS ] ", "getProductName : ", results);
     if(results.productName){
-      let pname = session.privateConversationData.productName = results.productName;
-      session.endConversation(`searching for ${pname} ...`);
+      let productName = session.privateConversationData.productName = results.productName;
+      console.log(`searching for ${productName} ...`);
+      session.beginDialog('getMinimumRating', {productName});
     }
     else{
       // no valid response received - End the conversation
       session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
     }
+  },
+  // extract minRating for product
+  (session, results, next) => {
+    console.log("[ LOGS ] ", "getMinRating : ", results);
+    if(results.minRating){
+      let minRating = session.privateConversationData.minRating = results.minRating;
+      session.endConversation(`take next input`);
+    }
+    else{
+      /* design note:
+      *  since all inputs except the product name are optional, even if the user
+      *  does not provide a valid input for the question, we should not end the
+      *  conversation
+      */
+      session.endConversation(`processing minRating input... ending conversation ...`);
+    }
   }]);
 
-  // simple dialog to get product user is looking for
+  // get product name, category, type
   bot.dialog('getProduct',[
     (session, args, next) => {
       // retreive args , if any
       if(args){
         session.dialogData.isReprompt = args.isReprompt;
       }
-      builder.Prompts.text(session, 'what are you looking for today ?');
+      builder.Prompts.text(session, 'What are you looking for today ?');
     },
     (session, results , next) => {
-      productName = session.dialogData.productName = results.response;
+      // exract product name for validation
+      let productName = session.dialogData.productName = results.response;
+        console.log("[ LOGS ] ", "productName - " , productName )
       /* design note :
       *  we should have a list of products that we support searching for
       *  this list should be used for validating user input in this dialog
@@ -67,8 +88,36 @@
           }
       }
       else{
-        session.send("great, let me look it up for you")
         session.endDialogWithResult({productName});
+      }
+    }
+  ]);
+
+  // get min rating for product search
+  bot.dialog('getMinimumRating',[
+    (session, args, next) => {
+        if(args){
+          session.dialogData.isReprompt = args.isReprompt;
+        }
+        builder.Prompts.number(session, 'what is the minimum user rating you want for the product?');
+    },
+    (session, results, next) =>{
+      // extract user rating for validation
+      let minRating = results.response;
+      console.log("[ LOGS ] ", "minRating - " , minRating )
+      if(!minRating || minRating < 0 || minRating > 5){
+        if(session.dialogData.isReprompt){
+           // already prompted once , close now
+           session.endDialogWithResult({ minRating: ''});
+        }
+        else{
+          session.send("minRating should be between 1-5")
+          // starting over again
+          session.replaceDialog('getMinimumRating', {isReprompt : true});
+        }
+      }
+      else{
+        session.endDialogWithResult({minRating});
       }
     }
   ]);
