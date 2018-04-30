6b6ac7543eee923911ed575416f6bf201c31f849
Nitinkumar Gove <gove.nitinkumar@gmail.com>
2018-04-29 17:41:33 -0700

    added LUIS recognizer

--- bot/config/config.js
+++ bot/config/config.js
@@ -1,3 +1,4 @@
+const LUIS_MODEL_URL=" 	https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/dcb06acd-b39f-42bf-a75e-d7f78fc483f7?subscription-key=85200804577849cb97c18a5cf621d36c&verbose=true&timezoneOffset=0&q="
 module.exports = {
-  API_TOKEN
+  LUIS_MODEL_URL
 };
